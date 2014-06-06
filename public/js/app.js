0599647aafab359a806899ec21056890f32317e4
Chris Compton <code@chriscompton.me>
2014-06-06 15:04:24 -0500

    Converting to Angular

--- public/js/app.js
+++ public/js/app.js
@@ -1,28 +1,79 @@
-$(function() {
-    var BDApp = Ember.Application.create({
-      LOG_TRANSITIONS: true
-    });
+angular.module('backdraft', [
+	'ngRoute', 'ngAnimate', 'bdServices', 'backdraft.services', 'backdraft.controllers'
+])
 
-    BDApp.ApplicationSerializer = DS.ActiveModelSerializer.extend({
-      primaryKey: '_id'
-    });
+.config(function ($compileProvider) {
+		$compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel):/);
+	})
 
-    BDApp.ApplicationController = Ember.Controller.extend({
-      authenticated: false,
-      checkLogin: function () {
-        if (!this.get('authenticated')) {
-          this.controllerFor('login').transitionToRoute('login');
+.config(function ($routeProvider, $locationProvider, $httpProvider) {
+    var isAuthenticated = function($q, $timeout, $http, $location, $rootScope) {
+      var deferred = $q.defer();
+      $http.get(urlBase + '/authenticated').success(function(auth){
+        if (auth !== '') {
+          $timeout(deferred.resolve, 0);
+        } else {
+          $rootScope.message = "You must log in.";
+          $timeout(function(){deferred.reject();}, 0);
+          $location.url('/login');
         }
+      });
+      return deferred.promise;
+    };
+
+
+
+		$routeProvider.when('/account', {
+			templateUrl: 'account.html',
+			controller: 'AppCtrl',
+      resolve: {
+        loggedin: isAuthenticated
       }
-    });
+		});
 
-    BDApp.ApplicationAdapter = DS.RESTAdapter.extend({
-      namespace: 'api' //,
-      //host: 'http://127.0.0.1:3000'
-    });
+		$routeProvider.when('/register', {
+			templateUrl: 'register.html',
+			controller: 'LoginCtrl'
+		});
 
-    
-    Models(BDApp);
-    Controllers(BDApp);
-    Routes(BDApp);
-});
+		$routeProvider.when('/login', {
+			templateUrl: 'login.html',
+			controller: 'LoginCtrl'
+		});
+
+		$routeProvider.otherwise({
+			redirectTo: '/login'
+		});
+
+  // Intercept 401 responses and redirect to login screen
+		$httpProvider.interceptors.push(function ($q, $location, AppAuth) {
+			return {
+				responseError: function (rejection) {
+					
+					console.log('intercepted rejection of ',
+							rejection.config.url, rejection.status);
+					
+					if (rejection.status === 401) {
+						AppAuth.currentUser = null;
+						// save the current location so that login can redirect back
+						$location.nextAfterLogin = $location.path();
+						$location.path('/login');
+					}
+					return $q.reject(rejection);
+				}
+			};
+		});
+	})
+
+.run(function ($rootScope, $location, AppAuth) {
+		$rootScope.$on('$routeChangeStart', function (event, next, current) {
+			console.log('AppAuth.currentUser', AppAuth.currentUser);
+			console.log('$location.path()', $location.path());
+		});
+
+    $rootScope.logout = function() {
+      $rootScope.message = "Logged out.";
+      $http.post('/logout');
+    }
+	});
+
--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,80 +1,55 @@
-var Controllers = function Controllers (BDApp) {
-  console.log("Controllers Loaded");
+angular.module('backdraft.controllers', ['backdraft.services'])
 
-  BDApp.ItemsController = Ember.ArrayController.extend({
-    sortProperties: ['title'],
-    sortAscending: true,
-    itemsCount: Ember.computed.alias('length'),
-    addForm: function () {
-      return this.store.createRecord('item', {
-        item: this.get('model')
-      });
-    }.property('model'),
-    actions: {
-      add: function () {
-        console.log('add called');
-        var controller = this;
-        this.get('addForm').set('description', '');
-        this.get('addForm').save().then(function (item) {
-          controller.get('model.items').addObject(item);
-        });
-      }
-    }
-  });
+.controller('AppCtrl', function ($scope, User, $location, AppAuth) {
+    AppAuth.ensureHasCurrentUser(User);	
+    $scope.currentUser = AppAuth.currentUser;
+			
+	$scope.options = [
+			{text: 'Logout', action: function () {
+				User.logout(function () {
+					$scope.currentUser =
+					AppAuth.currentUser = null;
+					$location.path('/');
+				});
+			}}
+		];
+	
+	$scope.toggleLeft = function () {
+			$scope.sideMenuController.toggleLeft();
+		};
+})
 
-  BDApp.AccountController = Ember.ArrayController.extend({
-    isAuthenticated: true,
-    booboo: 'hi',
-    user: function() {
-      return {lastName: "TEST", firstName: "TESTER"}
-    }
-  });
-
-  BDApp.LoginController = Ember.ArrayController.extend({
-    actions: {
-      login: function () {
-        console.log('login called');
-        var login = this.store.createRecord('auth', {
-          email: this.get('email'),
-          password: this.get('password')
-        });
-
-        var controller = this;
-
-        login.save().then(function (auth) {
-          console.log('Log in!');
-          controller.controllerFor('application').set('authenticated', true);
-          controller.transitionToRoute('account');
-        });        
-      },
-      logout: function () {
-        console.log('logout called');
-      },
-      check: function () {
-        console.log('check login');
-      }
-    }
-  });
-
-  BDApp.RegisterController = Ember.ArrayController.extend({
-    actions: {
-      register: function () {
-        console.log('register called');
-
-        var registration = this.store.createRecord('registration', {
-          firstName: this.get('firstName'),
-          lastName: this.get('lastName'),
-          email: this.get('email'),
-          password: this.get('password')
-        });
-
-        var controller = this;
-
-        registration.save().then(function (registration) {
-          console.log('Time to log in');
-          controller.transitionToRoute('account');
-        });
-      }
-    }
-  });
-};
+.controller('LoginCtrl', function ($scope, $routeParams, User, $location, AppAuth) {
+		$scope.registration = {};
+		$scope.credentials = {
+			email: 'foo@bar.com',
+			password: '123456'
+		};
+	
+		$scope.login = function () {
+			
+			$scope.loginResult = User.login({}, $scope.credentials,
+				function () {
+					var next = $location.nextAfterLogin || '/account';
+					$location.nextAfterLogin = null;
+					AppAuth.currentUser = $scope.loginResult.id;
+					$location.path(next);
+				},
+				function (res) {
+					$scope.loginError = res.data.error;
+				}
+			);
+		};
+			
+		$scope.register = function () {
+			$scope.user = User.create({registration: $scope.registration},
+				function () {
+					$location.path('/login');
+				},
+				function (res) {
+					$scope.registerError = res.data.error;
+				}
+			);
+		};
+			
+	});
